1a3cbf752bd472c68e9d752c52c1a31992b2fb22
kamalini26 <43268168+kamalini26@users.noreply.github.com>
2021-07-13 20:36:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react-dom"
+import Button from "./Button"
+import "./style.css"
+function App(){
+    return (
+    <div>
+        <h1>Hello World</h1>
+        <p>Great to learn new stuff</p>
+        <Button buttonText="Let's get started"/>
+    </div>
+    )
+}
+export default App
